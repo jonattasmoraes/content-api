42e1b820f50d5714daa3ec2f953550fa322e8bd7
Jônattas Moraes <jonatas.moraes.09@hotmail.com>
2024-01-31 19:42:21 -0300

    chore: add prisma schema

---
+++ prisma/migrations/20240131224026_init/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "api_content" (
+    "id" TEXT NOT NULL,
+    "aboutPage" TEXT NOT NULL,
+
+    CONSTRAINT "api_content_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "api_projects" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "year" TEXT NOT NULL,
+    "urlIcon" TEXT NOT NULL,
+    "aboutProject" TEXT NOT NULL,
+    "infos" TEXT NOT NULL,
+    "repositoryUrl" TEXT NOT NULL,
+    "deployUrl" TEXT NOT NULL,
+
+    CONSTRAINT "api_projects_pkey" PRIMARY KEY ("id")
+);
